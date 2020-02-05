4fadef49b8c50b6f0f573ff8ad790dcd1beb2eb1
Kevin <kramirez234@west-mec.org>
2020-02-05 16:48:51 -0700

    Finished case 1 without bugs

--- case1/tc_cart.js
+++ case1/tc_cart.js
@@ -17,7 +17,10 @@ var orderTotal = 0;
 var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
 
 for(var i = 0; i < item.length; i++){
-   cartHTML += "<tr><td><img src='tc_item.png' alt='item' /></td>";
+   cartHTML += "<tr><td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "' /></td>";
+   cartHTML += "<td>" + itemDescription[i] + "</td>";
+   cartHTML += "<td>$" + itemPrice[i] + "</td>" ;
+   cartHTML += "<td>" + itemQty + "</td>";
 var itemCost = itemPrice[i] * itemQty[i];
 
 cartHTML += "<td> $" + itemCost + "</td></tr>";
